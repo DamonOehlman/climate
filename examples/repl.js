fb55bd36e384763b5be53f52d4c174bafa65c1af
Damon Oehlman <damon.oehlman@sidelab.com>
2012-01-12 23:00:30 +1000

    repl fixes, bump version (0.0.2)

--- examples/repl.js
+++ examples/repl.js
@@ -4,4 +4,12 @@ cleave
     .repl('prompt>')
     .command('what', function(args) {
         console.log(args);
+    })
+    .command('wait', function(time) {
+        var waitTime = parseInt(time || 50, 10);
+        console.log('waiting for ' + waitTime);
+        
+        return function(callback) {
+            setTimeout(callback, waitTime);
+        };
     });
